cd4330f22d6d081c2ff81d89e7b3dad94b0c14f6
Ahmed3mad <3umduck@gmail.com>
2022-12-27 11:26:27 +0200

    first commit

---
+++ facebook_auth_platform_interface/lib/src/facebook_permissions.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/foundation.dart';
+
+/// this class is used to saved the granted and declined permissions after login
+class FacebookPermissions {
+  /// save the granted permissions by the user
+  final List<String> granted;
+
+  /// save the declined permissions by the user
+  final List<String> declined;
+
+  FacebookPermissions({@required this.granted, @required this.declined});
+}
